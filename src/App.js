a52e00cb34915eee2e2c67c46f00caf005f6e242
Irfaan Braaf <222207450@mycput.ac.za>
2026-04-13 14:37:28 +0200

    Removed the all non-functional components and files

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 import { useState } from "react";
 import MainPage from "./views/MainPage";
-import Experience from "./views/Experience";
-import Projects from "./views/Projects";
-import Skills from "./views/Skills";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   const [activePage, setActivePage] = useState("About Me");
+  const [externalCommand, setExternalCommand] = useState("");
 
-  const renderContent = () => {
-    switch (activePage) {
-      case "About Me":
-        return <MainPage />;
-      case "Experience":
-        return <Experience />;
-      case "Projects":
-        return <Projects />;
-      case "Skills":
-        return <Skills />;
-      default:
-        return <MainPage />;
-    }
+  const handleNavigate = (item) => {
+    setActivePage(item);
+
+    const command = item === "About Me" ? "about" : item.toLowerCase();
+    setExternalCommand(command);
+  };
+
+  const handleCommandProcessed = () => {
+    setExternalCommand("");
   };
 
   return (
     <div style={{ display: "flex", minHeight: "100vh", scrollbarGutter: "stable", scrollBehavior: "smooth" }}>
-      <Sidebar activePage={activePage} onNavigate={setActivePage} />
-      {renderContent()}
+      <Sidebar activePage={activePage} onNavigate={handleNavigate} />
+      <MainPage externalCommand={externalCommand} onCommandProcessed={handleCommandProcessed} />
     </div>
   );
 }
